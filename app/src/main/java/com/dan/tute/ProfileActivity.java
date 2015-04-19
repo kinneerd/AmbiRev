6cf9b04d0dc9cbe27e5fe0c485d4c50315359a18
Ande Misenheimer <Misenheimeram@appstate.edu>
2015-04-19 17:23:58 -0400

    Added message sending functionality to the send request button/text

--- app/src/main/java/com/dan/tute/ProfileActivity.java
+++ app/src/main/java/com/dan/tute/ProfileActivity.java
@@ -1,15 +1,19 @@
 package com.dan.tute;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.text.InputType;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.RatingBar;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -31,6 +35,8 @@ public class ProfileActivity extends ActionBarActivity {
 
     private String currentEmail;
     private String profileEmail;
+
+    private String message;
 
     private String url_load_tutor_profile = "http://68.119.36.255/tute/load_tutor_profile.php";
     private String url_send_request = "http://68.119.36.255/tute/requestEmailer.php";
@@ -64,7 +70,7 @@ public class ProfileActivity extends ActionBarActivity {
         mSendEmail.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                new SendEmailRequestActivity().execute();
+                sendMessage();
             }
         });
 
@@ -90,6 +96,39 @@ public class ProfileActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void sendMessage(){
+        message = "";
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Tutor Request");
+
+        // Set up the input
+        final EditText input = new EditText(this);
+        // Specify the type of input expected; this, for example, sets the input as a password, and will mask the text
+        input.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_FLAG_MULTI_LINE);
+        builder.setView(input);
+
+        // Set up the buttons
+        builder.setPositiveButton("Send", new DialogInterface.OnClickListener() {
+            @Override
+
+            public void onClick(DialogInterface dialog, int which) {
+                message = input.getText().toString();
+
+                new SendMessageRequestActivity().execute();
+            }
+        });
+
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+
+        builder.show();
     }
 
     class LoadProfileInformationActivity extends AsyncTask<String, String, String> {
@@ -126,7 +165,6 @@ public class ProfileActivity extends ActionBarActivity {
                             //phpMessage = json.getString("message");
                         }
                     } catch (JSONException e) {
-                        Log.d("Boogityboo", "bby");
                     }
                 }
             });
@@ -139,7 +177,7 @@ public class ProfileActivity extends ActionBarActivity {
         }
     }
 
-    class SendEmailRequestActivity extends AsyncTask<String, String, String> {
+    class SendMessageRequestActivity extends AsyncTask<String, String, String> {
         protected void onPreExecute(){
             super.onPreExecute();
         }
@@ -149,7 +187,7 @@ public class ProfileActivity extends ActionBarActivity {
             List<NameValuePair> params = new ArrayList<NameValuePair>();
             params.add(new BasicNameValuePair("emailTo", profileEmail));
             params.add(new BasicNameValuePair("emailFrom", currentEmail));
-            params.add(new BasicNameValuePair("message", "FILLER MESSAGE UNTIL WE MAKE A TEXTBOX."));
+            params.add(new BasicNameValuePair("message", message));
 
             final JSONObject json = jsonParser.makeHttpRequest(url_send_request, "POST", params);
 
@@ -167,7 +205,6 @@ public class ProfileActivity extends ActionBarActivity {
 
                         }
                     } catch (JSONException e) {
-                        Log.d("Boogityboo", "bby");
                     }
                 }
             });
@@ -181,7 +218,7 @@ public class ProfileActivity extends ActionBarActivity {
             Toast toast;
 
 
-            CharSequence text = "FILLER TEXT!";
+            CharSequence text = "Request Sent!";
             toast = Toast.makeText(context, text, duration);
             toast.show();
 
